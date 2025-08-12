a579da5d55c20f31773e353f77b4dbf998b46f58
RoberB1 <150208498+RoberB1@users.noreply.github.com>
2025-08-12 00:27:10 +0000

    Methods general Setup

--- api/ecosystems.php
+++ api/ecosystems.php
@@ -1,9 +1,24 @@
 <?php
 require_once __DIR__ . '/../app/controllers/EcosystemController.php';
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  EcosystemController::store();
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+if ($method === 'POST') {
+  if (isset($_GET['action']) && $_GET['action'] === 'update' && isset($_GET['id'])) {
+    EcosystemController::update((int)$_GET['id']);
+  } else {
+    EcosystemController::store();
+  }
+} elseif ($method === 'GET') {
+  if (isset($_GET['action']) && $_GET['action'] === 'edit' && isset($_GET['id'])) {
+    EcosystemController::edit((int)$_GET['id']);
+  } else {
+    http_response_code(405);
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode(['error' => 'Método GET no permitido.']);
+  }
 } else {
   http_response_code(405);
   header('Content-Type: application/json; charset=utf-8');
-  echo json_encode(['error' => 'Método no permitido. Usa POST.']);
+  echo json_encode(['error' => 'Método no permitido. Usa POST o GET.']);
 }
