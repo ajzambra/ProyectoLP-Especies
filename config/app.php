54bce45f6b1531178049fcf204b0cef5548e0dda
RoberB1 <150208498+RoberB1@users.noreply.github.com>
2025-08-24 03:09:08 +0000

    Fixex

--- config/app.php
+++ config/app.php
@@ -1,2 +1,10 @@
 <?php
 define('BASE_URL', '/ProyectoLP-Especies');
+
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
